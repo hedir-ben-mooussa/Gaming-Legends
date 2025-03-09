01373a06132f27b924aa62dbcc01cca70ac69109
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2025-03-09 01:14:24 +0100

    initial commit

---
+++ src/app/frontoffice/frontoffice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontoffice',
+  imports: [],
+  templateUrl: './frontoffice.component.html',
+  styleUrl: './frontoffice.component.scss'
+})
+export class FrontofficeComponent {
+
+}
